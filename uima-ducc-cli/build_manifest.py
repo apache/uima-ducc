63ee0626687114517f210650da5a68dbfda1511e
Jim Challenger <challngr@apache.org>
2013-01-23 14:04:40 +0000

    UIMA-2575 Correct version numbers for classpath in executable jar manifest.
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1437398 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-cli/build_manifest.py
+++ uima-ducc-cli/build_manifest.py
@@ -32,12 +32,12 @@ def main():
         "apache-camel/camel-xmlbeans-${org.apache.camel.version}.jar",
         "apache-camel/camel-xstream-${org.apache.camel.version}.jar",
         "apache-log4j/log4j-${log4j.version}.jar",
-        "apache-commons-cli/commons-cli-1.2.jar",
+        "apache-commons-cli/commons-cli-${commons.cli.version}.jar",
         "apache-activemq/activemq-all-${org.apache.activemq.version}.jar",
         "springframework/slf4j-log4j12-${slf4j.version}.jar",
         "springframework/slf4j-api-${slf4j.version}.jar",
         "springframework/jcl-over-slf4j-${slf4j.version}.jar",
-        "springframework/com.springsource.net.sf.cglib-2.2.0.jar",
+        "springframework/com.springsource.net.sf.cglib-${cglib.version}.jar",
         "springframework/aopalliance-${aopalliance.version}.jar",
         "springframework/spring-aop-${org.springframework.version}.jar",
         "springframework/spring-asm-${org.springframework.version}.jar",
@@ -48,8 +48,8 @@ def main():
         "springframework/spring-expression-${org.springframework.version}.jar",
         "springframework/spring-jms-${org.springframework.version}.jar",
         "springframework/spring-tx-${org.springframework.version}.jar",
-        "uima/uima-core-2.4.1-20121211.001650-31.jar",
-        "uima/uimaj-as-core-2.4.1-20130110.191457-4.jar",
+        "uima/uima-core-${org.apache.uima.version}.jar",
+        "uima/uimaj-as-${org.apache.uima.as.version}.jar",
         ]
 
     print ' '.join(cp)
