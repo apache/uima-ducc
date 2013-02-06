0fde7e5b53dc430936205e6d7099080148c76b8a
Jim Challenger <challngr@apache.org>
2013-02-06 20:38:06 +0000

    UIMA-2631 Remove unused import.
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1443196 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-web/src/main/java/org/apache/uima/ducc/ws/server/DuccFile.java
+++ uima-ducc-web/src/main/java/org/apache/uima/ducc/ws/server/DuccFile.java
@@ -24,7 +24,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-import org.apache.uima.ducc.common.IDuccEnv;
 import org.apache.uima.ducc.transport.event.common.IDuccWorkJob;
 
 public class DuccFile {
