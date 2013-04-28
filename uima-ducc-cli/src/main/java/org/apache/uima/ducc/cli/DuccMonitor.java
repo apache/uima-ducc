c5fd5a9ad948572239feaedc63b9ca63262f5fc0
Lou DeGenaro <degenaro@apache.org>
2013-04-28 10:01:15 +0000

    UIMA-2793 DUCC command line interface (CLI) does not honor "unified" --CancelOnInterrupt flag
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1476746 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-cli/src/main/java/org/apache/uima/ducc/cli/DuccMonitor.java
+++ uima-ducc-cli/src/main/java/org/apache/uima/ducc/cli/DuccMonitor.java
@@ -91,6 +91,7 @@ public abstract class DuccMonitor  {
         UiOption.Quiet, 
         UiOption.Timestamp, 
         UiOption.JobId,
+        UiOption.CancelOnInterrupt,
         UiOption.CancelJobOnInterrupt,
     };
     
