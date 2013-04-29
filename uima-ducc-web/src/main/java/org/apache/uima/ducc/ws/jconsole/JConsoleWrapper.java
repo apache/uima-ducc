e02f72532f8a317b524dceb05492738a6ae0b893
Lou DeGenaro <degenaro@apache.org>
2013-04-29 21:45:00 +0000

    UIMA-2852 DUCC webserver (WS) JConsole links not working
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1477362 13f79535-47bb-0310-9956-ffa450edef68

---
+++ uima-ducc-web/src/main/java/org/apache/uima/ducc/ws/jconsole/JConsoleWrapper.java
@@ -0,0 +1,9 @@
+package org.apache.uima.ducc.ws.jconsole;
+
+public class JConsoleWrapper {
+
+	public static void main(String[] args) {
+		sun.tools.jconsole.JConsole.main(args);
+	}
+
+}
---
+++ uima-ducc-web/src/main/java/sun/tools/jconsole/JConsole.java
@@ -0,0 +1,12 @@
+package sun.tools.jconsole;
+
+public class JConsole {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Bogus ... simply for compilation purposes
+	}
+
+}
