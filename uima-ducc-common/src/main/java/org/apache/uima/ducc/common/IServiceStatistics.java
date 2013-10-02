fb3abd2d70d53b9c308f48892a0881da9ca52b6c
Jim Challenger <challngr@apache.org>
2013-10-02 21:19:41 +0000

    UIMA-2682 Update javadoc.
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1528630 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-common/src/main/java/org/apache/uima/ducc/common/IServiceStatistics.java
+++ uima-ducc-common/src/main/java/org/apache/uima/ducc/common/IServiceStatistics.java
@@ -11,38 +11,45 @@ public interface  IServiceStatistics
 {
 
     /**
-     * Query whether the service is alive.
+     * Query whether the service is alive. This is used internally by the Service Manager.
+     *
      * @return "true" if the service is responsive, "false" otherwise.
      */
     public boolean isAlive();
 
     /**
-     * Query wether the service is "healthy".
+     * Query wether the service is "healthy". This is used internally by the Service Manager.
      * @return "true" if the service is healthy, "false" otherwise.
      */
     public boolean isHealthy();
 
     /**
-     * Return service statistics, if any.
-     * @return A string containing information regarding the service.  This is used only for display in the web server.
+     * Return service statistics, if any. This is used internally by the Service Manager.
+     * @return A string containing information regarding the service. 
      */
     public String  getInfo();
 
     /**
-     * Set the "aliveness" of the service.  This is called by each pinger for each service.
+     * Set the "aliveness" of the service.  This is called by each pinger for each service.  Set
+     *  this to return "true" if the service is responsive.  Otherwise return "false" so the Service
+     *  Manager can reject jobs dependent on this service.
      * @param alive Set to "true" if the service is responseve, "false" otherwise.
      */
     public void setAlive(boolean alive);
  
     /**
-     * Set the "health" of the service.  This is called by each pinger for each service.
+     * Set the "health" of the service.  This is called by each pinger for each service.  This is a
+     * subject judgement made by the service owner on behalf of his own service.  This is used only
+     * to reflect status in the DUCC Web Server.
      * @param healthy Set to "true" if the service is healthy, "false" otherwise.
      */
     public void setHealthy(boolean healthy);
 
     /**
-     * Set the monitor statistics for the service.  This is called by each pinger for each service.
-     * @param info This is an arbitrary string summarizing the service's performance.  This is used only in the web serverl
+     * Set the monitor statistics for the service. This is any arbitray string describing critical
+     * or useful characteristics of the service.  This string is presented as a "hover" in the
+     * webserver over the "health" field.
+     * @param info This is an arbitrary string summarizing the service's performance. 
      */
     public void setInfo(String info);
 
