0165aa4fb06d126926c068006924ba821df56756
Jaroslaw Cwiklik <cwiklik@apache.org>
2013-11-07 22:42:50 +0000

    UIMA-3419 Added Apache headers
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1539852 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-common/src/main/java/org/apache/uima/ducc/Workitem.java
+++ uima-ducc-common/src/main/java/org/apache/uima/ducc/Workitem.java
@@ -1,4 +1,21 @@
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+*/
 
 /* First created by JCasGen Fri Jul 19 18:26:47 EDT 2013 */
 package org.apache.uima.ducc;
@@ -10,10 +27,7 @@ import org.apache.uima.jcas.cas.TOP_Type;
 import org.apache.uima.jcas.cas.TOP;
 
 
-/** 
- * Updated by JCasGen Fri Aug 02 09:06:21 EDT 2013
- * XML source: /users1/eae/workspace-ducc/uima-ducc/uima-ducc-common/src/main/resources/org/apache/uima/ducc/common/uima/DuccJobFlowControlTS.xml
- * @generated */
+
 public class Workitem extends TOP {
   /** @generated
    * @ordered 
