9af299834aacb37e3f07ebae911a4b290e9ba2f5
Lou DeGenaro <degenaro@apache.org>
2014-02-01 11:46:08 +0000

    UIMA-3592 DUCC WebServer (WS) some pages missing data for logged-in user
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1563399 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-common/src/main/java/org/apache/uima/ducc/common/utils/AlienDirectory.java
+++ uima-ducc-common/src/main/java/org/apache/uima/ducc/common/utils/AlienDirectory.java
@@ -22,76 +22,56 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.zip.GZIPInputStream;
 
-public class AlienFile extends AlienAbstract {
+public class AlienDirectory extends AlienAbstract {
 	
 	private static DuccLogger duccLogger = DuccLoggerComponents.getWsLogger(AlienFile.class.getName());
 	
-	private String command_cat = "/bin/cat";
-	private String file_name;
+	private String command_ls = "/bin/ls";
+	private String command_ls_flags = "-atl";
+	private String directory_name;
 	
-	protected void set_command_cat(String value) {
-		command_cat = value;
+	protected void set_command_ls(String value) {
+		command_ls = value;
 	}
 	
-	protected String get_command_cat() {
-		return command_cat;
+	protected String get_command_ls() {
+		return command_ls;
 	}
 	
-	protected void set_file_name(String value) {
-		file_name = value;
+	protected void set_command_ls_flags(String value) {
+		command_ls_flags = value;
 	}
 	
-	protected String get_file_name() {
-		return file_name;
+	protected String get_command_ls_flags() {
+		return command_ls_flags;
 	}
 	
-	public AlienFile(String user, String file_name, String ducc_ling) {
+	protected void set_directory_name(String value) {
+		directory_name = value;
+	}
+	
+	protected String get_directory_name() {
+		return directory_name;
+	}
+	
+	public AlienDirectory(String user, String directory_name, String ducc_ling) {
 		set_user(user);
-		set_file_name(file_name);
+		set_directory_name(directory_name);
 		set_ducc_ling(ducc_ling);
 	}
 	
 	private String[] getCommand() {
-		String[] command_ducc_ling_yes = { ducc_ling, q_parameter, u_parameter, user, command_cat, file_name};
-		String[] command_ducc_ling_no  = { command_cat, file_name};
+		String[] command_ducc_ling_yes = { ducc_ling, q_parameter, u_parameter, user, command_ls, command_ls_flags, directory_name};
+		String[] command_ducc_ling_no  = { command_ls, command_ls_flags, directory_name};
 		String[] command = command_ducc_ling_yes;
 		if(ducc_ling == null) {
 			command = command_ducc_ling_no;
 		}
 		return command;
 	}
-	
-	private int getByteSize() throws Throwable {
-		String methodName = "getByteSize";
-		String[] command = getCommand();
-		echo(command);
-		ProcessBuilder pb = new ProcessBuilder( command );
-		Process p = pb.start();
-		//p.waitFor();
-		InputStream pOut = p.getInputStream();
-		InputStreamReader isr;
-		if(FileHelper.isGzFileType(file_name)) {
-			GZIPInputStream gis = new GZIPInputStream(pOut);
-			isr = new InputStreamReader(gis, encoding);
-		}
-		else {
-			isr = new InputStreamReader(pOut);
-		}
-		BufferedReader br = new BufferedReader(isr);
-		int size = 0;
-        int rc = 0;
-        while(rc >= 0) {
-			rc = br.read();
-	        size++;
-		}
-        String text = ""+size;
-        duccLogger.debug(methodName, duccId, text);
-        return size;
-	}
 
-	private String reader(int size) throws Throwable {
+	private String reader() throws Throwable {
 		String data = null;
 		BufferedReader br = null;
 		InputStreamReader isr = null;
@@ -102,17 +82,21 @@ public class AlienFile extends AlienAbstract {
 			Process p = pb.start();
 			//p.waitFor();
 			InputStream pOut = p.getInputStream();
-			if(FileHelper.isGzFileType(file_name)) {
-				GZIPInputStream gis = new GZIPInputStream(pOut);
-				isr = new InputStreamReader(gis, encoding);
-			}
-			else {
-				isr = new InputStreamReader(pOut);
-			}
-			char[] cbuf = new char[size];
+			isr = new InputStreamReader(pOut);
 	        br = new BufferedReader(isr);
-	        br.read(cbuf);
-	        data = new String(cbuf);
+	        StringBuilder sb = new StringBuilder();
+	        String line;
+	        boolean first = true;
+	        while ((line = br.readLine()) != null) {
+	        	if(first) {
+	        		first = false;
+	        	}
+	        	else {
+	        		sb.append(line);
+	        		sb.append("\n");
+	        	}
+	        }
+	        data = sb.toString();
 			return data;
 		}
 		catch(Throwable t) {
@@ -126,11 +110,9 @@ public class AlienFile extends AlienAbstract {
 	}
 	
 	public String getString() throws Throwable {
-		int size = getByteSize();
-		String data = reader(size);
-		if(data != null) {
-			data = data.trim();
-		}
+		String methodName = "getString";
+		String data = reader();
+		duccLogger.debug(methodName, duccId, data);
 		return data;
 	}
 	
@@ -143,13 +125,7 @@ public class AlienFile extends AlienAbstract {
 			Process p = pb.start();
 			//p.waitFor();
 			InputStream pOut = p.getInputStream();
-			if(FileHelper.isGzFileType(file_name)) {
-				GZIPInputStream gis = new GZIPInputStream(pOut);
-				isr = new InputStreamReader(gis, encoding);
-			}
-			else {
-				isr = new InputStreamReader(pOut);
-			}
+			isr = new InputStreamReader(pOut);
 		} 
 		catch(Throwable t) {
 			throw t;
@@ -175,17 +151,17 @@ public class AlienFile extends AlienAbstract {
 	}
 	
 	public static void main(String[] args) throws Throwable {
-		AlienFile alienFile;
+		AlienDirectory alienDirectory;
 		String arg_user = args[0];
-		String arg_file = args[1];
+		String arg_directory = args[1];
 		if(args.length < 3) {
-			alienFile = new AlienFile(arg_user, arg_file, null);
+			alienDirectory = new AlienDirectory(arg_user, arg_directory, null);
 		}
 		else {
 			String arg_ducc_ling = args[2];
-			alienFile = new AlienFile(arg_user, arg_file, arg_ducc_ling);
+			alienDirectory = new AlienDirectory(arg_user, arg_directory, arg_ducc_ling);
 		}
-		String data = alienFile.getString();
+		String data = alienDirectory.getString();
 		System.out.println(data);
 	}
 }
