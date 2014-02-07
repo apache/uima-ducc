e30673b573906d0cb3a04f2ff4a8450585fface3
Lou DeGenaro <degenaro@apache.org>
2014-02-07 13:35:04 +0000

    UIMA-3600 DUCC Job Driver (JD) does not properly handle work items for down node (RM Purged)
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1565643 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-common/src/main/java/org/apache/uima/ducc/common/utils/DuccPropertiesResolver.java
+++ uima-ducc-common/src/main/java/org/apache/uima/ducc/common/utils/DuccPropertiesResolver.java
@@ -112,6 +112,11 @@ public class DuccPropertiesResolver {
     public static final String ducc_ws_max_history_entries = "ducc.ws.max.history.entries";
     public static final String ducc_ws_login_enabled = "ducc.ws.login.enabled";
    
+    public static final String ducc_rm_node_stability = "ducc.rm.node.stability";
+    public static final String ducc_agent_node_metrics_publish_rate = "ducc.agent.node.metrics.publish.rate";
+    public static final String ducc_rm_state_publish_rate = "ducc.rm.state.publish.rate";
+    public static final String ducc_orchestrator_abbreviated_state_publish_rate = "ducc.orchestrator.abbreviated.state.publish.rate";
+    
     public static final String ducc_agent_node_inventory_publish_rate ="ducc.agent.node.inventory.publish.rate";
     public static final String ducc_agent_node_inventory_publish_rate_skip ="ducc.agent.node.inventory.publish.rate.skip";
     
