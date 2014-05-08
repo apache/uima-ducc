a596b5bb8e8f8e86b04874dd3ad3d10ba6f0b1d4
Jim Challenger <challngr@apache.org>
2014-05-08 17:56:36 +0000

    UIMA-3811 Fix entry into eviction loop.
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1593354 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-rm/src/main/java/org/apache/uima/ducc/rm/scheduler/Scheduler.java
+++ uima-ducc-rm/src/main/java/org/apache/uima/ducc/rm/scheduler/Scheduler.java
@@ -129,9 +129,11 @@ public class Scheduler
     // Bring up to speed with rest of ducc version. 2013-03-06 jrc
     //
     // 1.0.1 - RM can purge non-preemptables except for Unmanaged Reservations.  UIMA-3614
+    // 1.0.2 - vary-on, vary-off
+    // 1.0.3 - fix bad check in recursion in NodepoolScheduler.doEvictions
     final static int rmversion_major = 1;
     final static int rmversion_minor = 0;
-    final static int rmversion_ptf   = 1;  
+    final static int rmversion_ptf   = 3;  
     final static String rmversion_string = null;
 
     boolean initialized = false;           // we refuse nodeupdates until this is true
