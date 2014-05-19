7443799f3677c573d43f9752a07175713e7898e8
Jaroslaw Cwiklik <cwiklik@apache.org>
2014-05-19 17:59:43 +0000

    UIMA-3830 sum up memory used by processes owned by UID < max gid (500) and subtract it from the total memory available to the node.
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1595981 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-agent/src/main/java/org/apache/uima/ducc/agent/metrics/collectors/NodeMemInfoCollector.java
+++ uima-ducc-agent/src/main/java/org/apache/uima/ducc/agent/metrics/collectors/NodeMemInfoCollector.java
@@ -20,21 +20,25 @@ package org.apache.uima.ducc.agent.metrics.collectors;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import org.apache.uima.ducc.common.agent.metrics.memory.NodeMemory;
 import org.apache.uima.ducc.common.node.metrics.NodeMemoryInfo;
+import org.apache.uima.ducc.common.utils.Utils;
 
 
 public class NodeMemInfoCollector implements CallableMemoryCollector {
 	private long fakeMemorySize = -1;
 	private String[] targetFields;
-
+    private int gidMax = 500;   // default. Used to sum up memory of processes owned by uids < gidMax
 	public NodeMemInfoCollector(String[] targetFields) {
 		this.targetFields = targetFields;
 		String tmp;
+		
 		if ((tmp = System
 				.getProperty("ducc.agent.node.metrics.fake.memory.size")) != null) {
 			try {
@@ -43,6 +47,16 @@ public class NodeMemInfoCollector implements CallableMemoryCollector {
 				e.printStackTrace();
 			}
 		}
+		// SYSTEM_GID_MAX
+		if ((tmp = System
+				.getProperty("ducc.agent.node.metrics.sys.gid.max")) != null) {
+			try {
+				gidMax = Integer.valueOf(tmp);
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 	public NodeMemory call() throws Exception {
@@ -81,7 +95,46 @@ public class NodeMemInfoCollector implements CallableMemoryCollector {
 		} finally {
 			fileReader.close();
 		}
+		// sum up memory of all processes owned by UIDs < gidMax 
+		long memUsed = collectRSSFromPSCommand();
+		//System.out.println("Total:"+memInfoValues[0] + " Available:"+memInfoValues[1] +" Calculated:"+(memInfoValues[0] - memUsed)+" Priviledged Memory:"+memUsed);
+		memInfoValues[1] = memInfoValues[0] - memUsed;
 		return new NodeMemoryInfo(memInfoValues, fakeMemorySize);
+	}
+	private long collectRSSFromPSCommand() throws Exception {
+		InputStream stream = null;
+	    BufferedReader reader = null;
+	  
+	      ProcessBuilder pb;
+	      if ( Utils.isMac() ) {
+	        pb = new ProcessBuilder("ps","-Ao","user=,pid=,uid=,rss=");
+	      } else {
+	        pb = new ProcessBuilder("ps","-Ao","user:12,pid,uid,rss", "--no-heading");
+	      }
+	      pb.redirectErrorStream(true);
+	      Process proc = pb.start();
+	      //  spawn ps command and scrape the output
+	      stream = proc.getInputStream();
+	      reader = new BufferedReader(new InputStreamReader(stream));
+	      String line;
+	      String regex = "\\s+";
+	      long memoryUsed = 0;
+	      // read the next line from ps output
+	      while ((line = reader.readLine()) != null) {
+	          String tokens[] = line.split(regex);
+	          if ( tokens.length > 0 ) {
+	        	  try {
+	        		  int uid = Integer.valueOf(tokens[2]);
+	        		  if ( uid < gidMax ) {
+	        			  memoryUsed += Long.valueOf(tokens[3]);
+	        		  }
+	        	  } catch( NumberFormatException nfe) {
+	        		  
+	        	  }
+	          }
+	      }	
+	      stream.close();
+	      return memoryUsed;
 	}
 	public static void main(String[] args) {
 	    String[] meminfoTargetFields = new String[] {"MemTotal:","MemFree:","SwapTotal:","SwapFree:"};
@@ -92,8 +145,8 @@ public class NodeMemInfoCollector implements CallableMemoryCollector {
 			while( true ) {
 				Future<NodeMemory> nmiFuture = pool.submit(nmi);
 				NodeMemory memInfo = nmiFuture.get();
-				System.out.println("... Memonfo Data:"
-						+memInfo.getMemTotal()+
+				System.out.println("... Meminfo Data -"+
+						" Memory Total:"+memInfo.getMemTotal()+
 						" Memory Free:"+memInfo.getMemFree()+
 						" Swap Total:"+memInfo.getSwapTotal()+
 						" Swap Free:"+memInfo.getSwapFree());
