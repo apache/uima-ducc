a3db3e9f6aa98f4190e72b386cd1ae4d933f19b3
Lou DeGenaro <degenaro@apache.org>
2014-09-10 12:46:04 +0000

    UIMA-4006 DUCC Web Server (WS) NPE in DuccHandlerClassic
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1623982 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-web/src/main/java/org/apache/uima/ducc/ws/server/DuccWebUtil.java
+++ uima-ducc-web/src/main/java/org/apache/uima/ducc/ws/server/DuccWebUtil.java
@@ -145,62 +145,64 @@ public class DuccWebUtil {
 		boolean list = false;
 		DuccCookies.FilterUsersStyle filterUsersStyle = DuccCookies.getFilterUsersStyle(request);
 		if(!users.isEmpty()) {
-			String user = propertiesMeta.getProperty(key_user).trim();
-			if(user != null) {
-				user = user.trim();
-				switch(filterUsersStyle) {
-				case IncludePlusActive:
-					if(isAvailable(propertiesMeta)) {
-						list = true;
-					}
-					else if(users.contains(user)) {
-						if(maxRecords > 0) {
-							if (counter < maxRecords) {
-								list = true;
+			if(propertiesMeta != null) {
+				String user = propertiesMeta.getProperty(key_user);
+				if(user != null) {
+					user = user.trim();
+					switch(filterUsersStyle) {
+					case IncludePlusActive:
+						if(isAvailable(propertiesMeta)) {
+							list = true;
+						}
+						else if(users.contains(user)) {
+							if(maxRecords > 0) {
+								if (counter < maxRecords) {
+									list = true;
+								}
 							}
 						}
-					}
-					break;
-				case ExcludePlusActive:
-					if(isAvailable(propertiesMeta)) {
-						list = true;
-					}
-					else if(!users.contains(user)) {
-						if(maxRecords > 0) {
-							if (counter < maxRecords) {
-								list = true;
+						break;
+					case ExcludePlusActive:
+						if(isAvailable(propertiesMeta)) {
+							list = true;
+						}
+						else if(!users.contains(user)) {
+							if(maxRecords > 0) {
+								if (counter < maxRecords) {
+									list = true;
+								}
 							}
 						}
-					}
-					break;
-				case Include:
-					if(users.contains(user)) {
-						if(maxRecords > 0) {
-							if (counter < maxRecords) {
-								list = true;
+						break;
+					case Include:
+						if(users.contains(user)) {
+							if(maxRecords > 0) {
+								if (counter < maxRecords) {
+									list = true;
+								}
 							}
 						}
-					}
-					break;
-				case Exclude:
-					if(!users.contains(user)) {
-						if(maxRecords > 0) {
-							if (counter < maxRecords) {
-								list = true;
+						break;
+					case Exclude:
+						if(!users.contains(user)) {
+							if(maxRecords > 0) {
+								if (counter < maxRecords) {
+									list = true;
+								}
 							}
 						}
-					}
-					break;
-				}	
+						break;
+					}	
+				}
 			}
-		}
-		else {
-			if(isAvailable(propertiesMeta)) {
-				list = true;
-			}
-			else if(maxRecords > 0) {
-				if (counter < maxRecords) {
+			else {
+				if(isAvailable(propertiesMeta)) {
 					list = true;
+				}
+				else if(maxRecords > 0) {
+					if (counter < maxRecords) {
+						list = true;
+					}
 				}
 			}
 		}
