e90db9ca416eccaca97fd19afc01af51f3caf9c5
Jaroslaw Cwiklik <cwiklik@apache.org>
2015-06-24 13:59:08 +0000

    UIMA-4358 removed dead code
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1687283 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-transport/src/main/java/org/apache/uima/ducc/transport/configuration/jp/DuccHttpClient.java
+++ uima-ducc-transport/src/main/java/org/apache/uima/ducc/transport/configuration/jp/DuccHttpClient.java
@@ -28,16 +28,10 @@ import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.RequestEntity;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.apache.http.ConnectionReuseStrategy;
-import org.apache.http.HttpClientConnection;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.DefaultConnectionReuseStrategy;
 import org.apache.http.impl.pool.BasicConnPool;
 import org.apache.http.impl.pool.BasicPoolEntry;
-import org.apache.http.message.BasicHttpEntityEnclosingRequest;
-import org.apache.http.message.BasicHttpRequest;
 import org.apache.http.protocol.HttpCoreContext;
 import org.apache.http.protocol.HttpProcessor;
 import org.apache.http.protocol.HttpProcessorBuilder;
@@ -46,7 +40,6 @@ import org.apache.http.protocol.RequestConnControl;
 import org.apache.http.protocol.RequestContent;
 import org.apache.http.protocol.RequestTargetHost;
 import org.apache.http.protocol.RequestUserAgent;
-import org.apache.http.util.EntityUtils;
 import org.apache.uima.ducc.common.NodeIdentity;
 import org.apache.uima.ducc.common.utils.DuccLogger;
 import org.apache.uima.ducc.common.utils.XStreamUtils;
@@ -170,14 +163,6 @@ public class DuccHttpClient {
 		String pn = System.getenv("ProcessDuccIdFriendly");
 		return pn;
 	}
-    private void addCommonHeaders( BasicHttpRequest request ) {
-    	request.setHeader("IP", getIP());
-		request.setHeader("Hostname", getNodeName());
-		request.setHeader("ThreadID",
-				String.valueOf(Thread.currentThread().getId()));
-		request.setHeader("PID", pid);
-		
-    }
     private void addCommonHeaders( IMetaCasTransaction transaction ) {
     	String location = "addCommonHeaders";
     	transaction.setRequesterAddress(getIP());
@@ -203,91 +188,6 @@ public class DuccHttpClient {
 		
     }
 
-	public IMetaCasTransaction get(IMetaCasTransaction transaction) throws Exception {
-		// According to HTTP spec, GET request should not include the body. We need
-		// to send in body to the JD so use POST
-		BasicHttpEntityEnclosingRequest request = new BasicHttpEntityEnclosingRequest("POST", target);
-		addCommonHeaders(transaction);
-		addCommonHeaders(request);
-		return execute(request, transaction);
-	}
-	public IMetaCasTransaction post(IMetaCasTransaction transaction) throws Exception {
-		BasicHttpEntityEnclosingRequest request = new BasicHttpEntityEnclosingRequest("POST", target);
-		addCommonHeaders(transaction);
-		addCommonHeaders(request);
-		transaction.setDirection(Direction.Request);
-		return execute(request,transaction);
-	}
-	private IMetaCasTransaction execute( BasicHttpEntityEnclosingRequest request, IMetaCasTransaction transaction ) throws Exception {
-		BasicPoolEntry poolEntry=null;
-		int retry = 2;
-		Exception lastError = null;
-		IMetaCasTransaction reply=null;
-
-		while( retry-- > 0 ) {
-			try {
-				// Get the connection from the pool
-			    Future<BasicPoolEntry> future = connPool.lease(host,  null);
-			    poolEntry = future.get();
-			    
-			    HttpClientConnection conn = poolEntry.getConnection();
-			    coreContext.setAttribute(HttpCoreContext.HTTP_CONNECTION, conn);
-	            coreContext.setAttribute(HttpCoreContext.HTTP_TARGET_HOST, host);
-	            
-	            conn.setSocketTimeout(10000); 
-//				System.out.println(">> Request URI: "	+ request.getRequestLine().getUri());
-	           // request.
-
-				request.setHeader("content-type", "text/xml");
-	            String body = XStreamUtils.marshall(transaction);
-				HttpEntity entity = new StringEntity(body);
-
-				request.setEntity(entity);
-
-				System.out.println(">>>> GET Request:"+body);
-				
-				httpexecutor.preProcess(request, httpproc, coreContext);
-				HttpResponse response = httpexecutor.execute(request, conn,	coreContext);
-				httpexecutor.postProcess(response, httpproc, coreContext);
-
-				if ( response.getStatusLine().getStatusCode() != 200) {
-	//				System.out.println("Unable to Communicate with JD - Error:"+response.getStatusLine());
-					throw new RuntimeException("JP Http Client Unable to Communicate with JD - Error:"+response.getStatusLine());
-				}
-		//		System.out.println("<< Response: "
-		//				+ response.getStatusLine());
-				String responseData = EntityUtils.toString(response.getEntity());
-		//		System.out.println(responseData);
-				Object o = XStreamUtils.unmarshall(responseData);
-				if ( o instanceof IMetaCasTransaction) {
-					reply = (MetaCasTransaction)o;
-					break;
-				} else {
-					throw new InvalidClassException("Expected IMetaCasTransaction - Instead Received "+o.getClass().getName());
-				}
-			} catch( Exception t) {
-				lastError = t;
-				t.printStackTrace();
-			}
-			finally {
-			//	System.out.println("==============");
-				connPool.release(poolEntry, true);
-			}
-			
-		}
-		if ( reply != null ) {
-			return reply;
-		} else {
-			if ( lastError != null ){
-				throw lastError;
-
-			} else {
-				throw new RuntimeException("Shouldn't happen ");
-			}
-		} 
-	}
-
-	
 	public IMetaCasTransaction execute( IMetaCasTransaction transaction, PostMethod postMethod ) throws Exception {
 		int retry = 2;
 		Exception lastError = null;
@@ -300,7 +200,6 @@ public class DuccHttpClient {
 			try {
 				// Serialize request object to XML
 				String body = XStreamUtils.marshall(transaction);
-				//System.out.println("Sending Message of size:"+body.length()+" Message:"+body);
 	            RequestEntity e = new StringRequestEntity(body,"application/xml","UTF-8" );
 	            
 	            postMethod.setRequestEntity(e);
@@ -323,14 +222,12 @@ public class DuccHttpClient {
 				}
 				logger.debug("execute", null, "Thread:"+Thread.currentThread().getId()+" JD Reply Status:"+postMethod.getStatusLine());
 				logger.debug("execute", null, "Thread:"+Thread.currentThread().getId()+" Recv'd:"+content);
-				//System.out.println("Thread::"+Thread.currentThread().getId()+" " +content);
 				Object o;
 				try {
 					o = XStreamUtils.unmarshall(content); //sb.toString());
 					
 				} catch( Exception ex) {
-					System.out.println("Got Error Thread::"+Thread.currentThread().getId()+" ERRR::Content causing error:"+postMethod.getResponseBody());
-					
+					logger.error("execute", null, "Thread:"+Thread.currentThread().getId()+" ERRR::Content causing error:"+postMethod.getResponseBody(),ex);
 					throw ex;
 				}
 				if ( o instanceof IMetaCasTransaction) {
