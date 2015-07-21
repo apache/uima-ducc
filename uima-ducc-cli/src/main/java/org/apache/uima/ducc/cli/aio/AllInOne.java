e2217d9e2c91be7db06ba35a9e144e0cf80a911f
Burn L. Lewis <burn@apache.org>
2015-07-21 17:34:05 +0000

    UIMA-4521 Remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1692178 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-cli/src/main/java/org/apache/uima/ducc/cli/aio/AllInOne.java
+++ uima-ducc-cli/src/main/java/org/apache/uima/ducc/cli/aio/AllInOne.java
@@ -18,19 +18,14 @@
 */
 package org.apache.uima.ducc.cli.aio;
 
-import java.io.IOException;
-
-import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.impl.XmiSerializationSharedData;
-import org.apache.uima.collection.CollectionException;
 import org.apache.uima.ducc.cli.CliBase;
 import org.apache.uima.ducc.cli.DuccJobSubmit;
 import org.apache.uima.ducc.cli.aio.IMessageHandler.Level;
 import org.apache.uima.ducc.cli.aio.IMessageHandler.Toggle;
 import org.apache.uima.ducc.transport.event.cli.JobRequestProperties;
 import org.apache.uima.ducc.user.common.DuccUimaSerializer;
-import org.apache.uima.resource.ResourceInitializationException;
 
 public class AllInOne extends CliBase {
     
