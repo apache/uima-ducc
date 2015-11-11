e40c3c87f237a69904cd877f332f0b24a9715042
Jim Challenger <challngr@apache.org>
2015-11-11 18:45:13 +0000

    UIMA-4577 Updates to scripting.
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1713912 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/admin/db_util.py
+++ src/main/admin/db_util.py
@@ -70,6 +70,7 @@ def configure_database(DUCC_HOME, DUCC_HEAD, java, db_pw):
     here = os.getcwd()
     os.chdir(DUCC_HOME + "/cassandra-server")
     pidfile = DUCC_HOME + '/state/cassandra.pid'
+    print 'Starting the database.  This might take a few moments if it is the first time.'
     CMD = "bin/cassandra -p "+  pidfile + " > /dev/null 2>&1";
     os.system(CMD);
     print "Database is started.  Waiting for initialization";
