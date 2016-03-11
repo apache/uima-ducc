a780e47ce24091cad84216002944d7827b09c2fb
Lou DeGenaro <degenaro@apache.org>
2016-03-11 13:47:25 +0000

    UIMA-4838 Jenkins on Windows does not support POSIX
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1734558 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-common/src/test/java/org/apache/uima/ducc/common/test/CryptoTest.java
+++ uima-ducc-common/src/test/java/org/apache/uima/ducc/common/test/CryptoTest.java
@@ -22,6 +22,7 @@ import static org.junit.Assert.*;
 
 import java.util.Properties;
 
+import org.apache.commons.lang.SystemUtils;
 import org.apache.uima.ducc.common.crypto.Crypto;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -50,22 +51,28 @@ public class CryptoTest {
 	@Test
 	public void test() {
 		try {
-			String user = System.getProperty("user.name");
-			String home = System.getProperty("user.home");
-			Crypto crypto = new Crypto(user, true);
-			String message = "Hello DUCC!";
-			byte[] cypheredMessage = crypto.encrypt(message);
-			Properties properties = new Properties();
-			String key_signature = "signature";
-			properties.put(key_signature, cypheredMessage);
-			cypheredMessage = (byte[]) properties.get(key_signature);
-			Object decypheredMessage = crypto.decrypt(cypheredMessage);
-			if(!message.equals(decypheredMessage)) {
-				System.out.println("user="+user);
-				System.out.println("home="+home);
-				System.out.println("message="+(String)message);
-				System.out.println("decypher="+(String)decypheredMessage);
-				fail("decypher mismatch?!");
+			if(SystemUtils.IS_OS_WINDOWS) {
+		    	// Avoid unnecessary Jenkins failures
+		    	System.out.println("Windows does not support POSIX");
+		    }
+			else {
+				String user = System.getProperty("user.name");
+				String home = System.getProperty("user.home");
+				Crypto crypto = new Crypto(user, true);
+				String message = "Hello DUCC!";
+				byte[] cypheredMessage = crypto.encrypt(message);
+				Properties properties = new Properties();
+				String key_signature = "signature";
+				properties.put(key_signature, cypheredMessage);
+				cypheredMessage = (byte[]) properties.get(key_signature);
+				Object decypheredMessage = crypto.decrypt(cypheredMessage);
+				if(!message.equals(decypheredMessage)) {
+					System.out.println("user="+user);
+					System.out.println("home="+home);
+					System.out.println("message="+(String)message);
+					System.out.println("decypher="+(String)decypheredMessage);
+					fail("decypher mismatch?!");
+				}
 			}
 		}
 		catch(Exception e) {
