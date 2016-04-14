fc18c9b051c28f42aff817db323a3d8fcc71aaf5
Lou DeGenaro <degenaro@apache.org>
2016-04-14 18:34:13 +0000

    UIMA-4884 DUCC standardization of environment variables
    define enums
    
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/uima-ducc/trunk@1739163 13f79535-47bb-0310-9956-ffa450edef68

---
+++ uima-ducc-common/src/main/java/org/apache/uima/ducc/common/IDuccUser.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+*/
+package org.apache.uima.ducc.common;
+
+// Note: Coordinate with org.apache.uima.ducc.IUser
+
+public interface IDuccUser {
+
+	public enum EnvironmentVariable {
+		DUCC_HOME("DUCC_HOME"),
+		DUCC_ID_JOB("DUCC_JOBID"),
+		DUCC_ID_PROCESS("DUCC_PROCESSID"),
+		DUCC_ID_PROCESS_UNIQUE("DUCC_PROCESSID_UNIQUE"),
+		DUCC_ID_SERVICE("DUCC_SERVICE_INSTANCE"),
+		DUCC_LOG_PREFIX("DUCC_PROCESS_LOG_PREFIX"),
+		//
+		DUCC_IP("IP"),
+		DUCC_NODENAME("NodeName"),
+		USER("USER"),
+		;
+		
+		private String value = null;
+		
+		private EnvironmentVariable(String valueForKey) {
+			setValue(valueForKey);
+		}
+		
+		private void setValue(String valueForKey) {
+			value = valueForKey;
+		}
+		
+		private String getValue() {
+			return value;
+		}
+		
+		public String standard() {
+			return name();
+		}
+		
+		public String mapped() {
+			return getValue();
+		}
+		
+		public String value() {
+			String retVal = mapped();
+			switch(this) {
+			default:
+				break;
+			}
+			return retVal;
+		}
+	}
+	
+	
+}
