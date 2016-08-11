fdbfb3e1aa28e2c76fbcc72e6fb98246e826430d
Jaroslaw Cwiklik <cwiklik@apache.org>
2016-08-11 14:43:23 +0000

    UIMA-5048 modified PM to ignore tasks for which there is no command line
    git-svn-id: https://svn.apache.org/repos/asf/uima/uima-ducc/trunk@1755998 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-pm/src/main/java/org/apache/uima/ducc/pm/ProcessManagerComponent.java
+++ uima-ducc-pm/src/main/java/org/apache/uima/ducc/pm/ProcessManagerComponent.java
@@ -191,6 +191,14 @@ implements ProcessManager {
 	        case Job:
 	          logger.debug(methodName, dcj.getDuccId(), "case: Job");
 	          dw = dwHelper.fetch(dcj.getDuccId());
+	          // Aug 11, 2016 JIRA 5048
+	          // If the OR returns null from fetch() above, don't send anything to agents.
+	          // The dw contains a command line to launch processes. If its not provided 
+	          // there is nothing to do.
+	          if ( dw == null ) {
+	        	  logger.info(methodName, dcj.getDuccId(), "The OR did not provide commndline spec and other details required to launch processes for the Job. Received value of NULL from the OR. The PM will ignore this job and will not publish it to agents.");
+	        	  continue;  // nothing to do
+	          }
 	          IDuccWorkJob job = (IDuccWorkJob) dw;
 	          DuccWorkPopDriver driver = job.getDriver();
 			  if(driver != null) {
@@ -202,6 +210,11 @@ implements ProcessManager {
 	        case Service:
 	          logger.debug(methodName, dcj.getDuccId(), "case: Service");
 	          dw = dwHelper.fetch(dcj.getDuccId());
+	          if ( dw == null ) {
+	        	  logger.info(methodName, dcj.getDuccId(), "The OR did not provide commndline spec and other details required to launch Service processes. Received value of NULL from the OR. The PM will ignore this Service and will not publish to agents.");
+	        	  continue;  // nothing to do
+	          }
+
 	          IDuccWorkJob service = (IDuccWorkJob) dw;
 	          processCmdLine = service.getCommandLine();
 	          processCmdLine.addOption("-Dducc.deploy.components=service");
@@ -209,6 +222,11 @@ implements ProcessManager {
 	        default:
 	          logger.debug(methodName, dcj.getDuccId(), "case: default");
 	          dw = dwHelper.fetch(dcj.getDuccId());
+	          if ( dw == null ) {
+	        	  logger.info(methodName, dcj.getDuccId(), "The OR did not provide commndline spec and other details required to launch processes. Received value of NULL from the OR. The PM will ignore this task and will not publish to agents.");
+	        	  continue;  // nothing to do
+	          }
+
 	          if(dw instanceof IDuccWorkExecutable) {
 	        	  IDuccWorkExecutable dwe = (IDuccWorkExecutable) dw;
 	        	  processCmdLine = dwe.getCommandLine();
