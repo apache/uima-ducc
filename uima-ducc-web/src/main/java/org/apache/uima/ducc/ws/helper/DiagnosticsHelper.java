f3691bf84da25595b3457302c4355375038e0ec5
Lou DeGenaro <degenaro@apache.org>
2017-01-09 14:57:42 +0000

    UIMA-5232 DUCC missing DUCC Book Documentation > move ducc_disk_info from bin to admin
    git-svn-id: https://svn.apache.org/repos/asf/uima/uima-ducc/trunk@1777978 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-web/src/main/java/org/apache/uima/ducc/ws/helper/DiagnosticsHelper.java
+++ uima-ducc-web/src/main/java/org/apache/uima/ducc/ws/helper/DiagnosticsHelper.java
@@ -43,7 +43,7 @@ public class DiagnosticsHelper {
 			try {
 				String path = System.getProperty("DUCC_HOME")
 							+File.separator
-							+"bin"
+							+"admin"
 							+File.separator
 							+"ducc_disk_info"
 							;
