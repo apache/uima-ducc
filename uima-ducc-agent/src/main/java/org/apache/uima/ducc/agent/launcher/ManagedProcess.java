b729df0cb43d9eeb417ccc5d85eadbed096a98fa
Jaroslaw Cwiklik <cwiklik@apache.org>
2017-01-31 15:15:50 +0000

    UIMA-5284 modified to set reason=FailedInitialization if prcoess failed on its own while in Starting state
    git-svn-id: https://svn.apache.org/repos/asf/uima/uima-ducc/trunk@1781103 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-agent/src/main/java/org/apache/uima/ducc/agent/launcher/ManagedProcess.java
+++ uima-ducc-agent/src/main/java/org/apache/uima/ducc/agent/launcher/ManagedProcess.java
@@ -415,7 +415,7 @@ public class ManagedProcess implements Process {
 			} else {
 				if ( !isAP && !isstopping ) {
 					// check if process exited while in Initializing state  
-					if ( ProcessState.Initializing.equals(pstate) ) {
+					if ( ProcessState.Initializing.equals(pstate) || ProcessState.Starting.equals(pstate)) {
 						getDuccProcess().setReasonForStoppingProcess(ReasonForStoppingProcess.FailedInitialization.toString());
 		                log("ManagedProcess.drainProcessStreams",
 				    "Process Failed while in initializing state - setting reason to "+getDuccProcess().getReasonForStoppingProcess());
