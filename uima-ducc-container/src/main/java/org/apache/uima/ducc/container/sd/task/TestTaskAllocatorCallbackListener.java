f4609e179d9a0895bee55e9a115af11a20b5bb63
Burn L. Lewis <burn@apache.org>
2017-04-25 20:00:12 +0000

    UIMA-5404 Extend the task allocator test client to optionally read its work from a file
    git-svn-id: https://svn.apache.org/repos/asf/uima/uima-ducc/trunk@1792649 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-container/src/main/java/org/apache/uima/ducc/container/sd/task/TestTaskAllocatorCallbackListener.java
+++ uima-ducc-container/src/main/java/org/apache/uima/ducc/container/sd/task/TestTaskAllocatorCallbackListener.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -19,6 +19,10 @@
 
 package org.apache.uima.ducc.container.sd.task;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicLong;
@@ -34,12 +38,22 @@ import org.apache.uima.util.CasCreationUtils;
 import org.apache.uima.util.Level;
 import org.apache.uima.util.Logger;
 
+/*
+ * Creates tasks/work from an input file or sends a fixed string every time.
+ */
 public class TestTaskAllocatorCallbackListener implements
 		TaskAllocatorCallbackListener {
 	Logger logger = UIMAFramework.getLogger(TestTaskAllocatorCallbackListener.class);
 	private static DuccUimaSerializer uimaSerializer = new DuccUimaSerializer();
 	private AtomicLong seqno = new AtomicLong(0);
-	public TestTaskAllocatorCallbackListener() {
+    private BufferedReader inf;
+    private String text = "Some Text";
+	public TestTaskAllocatorCallbackListener() throws FileNotFoundException {
+	    String taskFilename = System.getProperty("TestTaskFile");
+	    if (taskFilename != null) {
+	        File taskFile = new File(taskFilename);
+	        inf = new BufferedReader(new FileReader(taskFile));
+	    }
 	}
 	public String serialize(CAS cas) throws Exception {
 		String serializedCas = uimaSerializer.serializeCasToXmi(cas);
@@ -52,24 +66,36 @@ public class TestTaskAllocatorCallbackListener implements
 			CAS cas = null;
 			cas = CasCreationUtils.createCas(new TypeSystemDescription_impl(), null, null);
 			cas.setDocumentLanguage("en");
-			cas.setDocumentText("Some Text");	
-//			cas.setDocumentText("100 "+seqno.incrementAndGet()+" 1000 0");	
-			
+			if (inf != null) {
+			    text = inf.readLine();
+			    if (text == null) {
+			        inf.close();
+			        inf = null;
+			    }
+			}
+			if (text == null) {
+			    logger.log(Level.INFO," ... no work avaialble");
+			    return null;
+			}
+			logger.log(Level.INFO,"delivering: " + text);
+			cas.setDocumentText(text);
+//			cas.setDocumentText("100 "+seqno.incrementAndGet()+" 1000 0");
+
 			serializedCas = serialize(cas);
 			cas.reset();
 			cas.release();
-			
+
 		} catch( Exception e) {
 			logger.log(Level.WARNING,"Error",e);
 		}
-		
+
 		return serializedCas;
 	}
 
 	public synchronized void onTaskSuccess(TaskConsumer taskConsumer, IPerformanceMetrics metrics) {
 		logger.log(Level.INFO,"onTaskSuccess() Called");
 		List<Properties> breakdown = metrics.get();
-		
+
 		for( Properties p : breakdown ) {
 			StringBuffer sb = new StringBuffer();
 			sb.append("AE Name: ").append(p.get("uniqueName")).append(" Analysis Time: ").append(p.get("analysisTime"));
