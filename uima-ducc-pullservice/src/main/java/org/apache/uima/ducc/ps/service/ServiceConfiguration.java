73cb9e57fadc65972e27cf4416eec2d99cbe53ad
Burn L. Lewis <burn@apache.org>
2018-05-23 13:57:40 +0000

    UIMA-5756 Added copyrights; added 2 setter methods to ServiceConfiguration & removed 1 validation check
    git-svn-id: https://svn.apache.org/repos/asf/uima/uima-ducc/trunk@1832102 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ducc-pullservice/src/main/java/org/apache/uima/ducc/ps/service/ServiceConfiguration.java
+++ uima-ducc-pullservice/src/main/java/org/apache/uima/ducc/ps/service/ServiceConfiguration.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+*/
+
 package org.apache.uima.ducc.ps.service;
 
 import org.apache.uima.ducc.ps.service.errors.ServiceInitializationException;
@@ -28,46 +47,41 @@ public class ServiceConfiguration {
 		return serviceJmxConnectURL;
 	}
 
-
 	public void setServiceJmxConnectURL(String serviceJmxConnectURL) {
 		this.serviceJmxConnectURL = serviceJmxConnectURL;
 	}
-
 
 	public String getClientURL() {
 		return clientURL;
 	}
 
-
 	public String getThreadCount() {
 		return threadCount;
 	}
 
+	public void setThreadCount(String threadCount) {
+		this.threadCount = threadCount;
+	}
 
 	public String getDuccHome() {
 		return duccHome;
 	}
 
-
 	public String getJobId() {
 		return jobId;
 	}
-
 
 	public String getDuccProcessId() {
 		return duccProcessId;
 	}
 
-
 	public String getDuccProcessUniqueId() {
 		return duccProcessUniqueId;
 	}
 
-
 	public String getMonitorPort() {
 		return monitorPort;
 	}
-
 
 	public String getAnalysisEngineDescriptorPath() {
 		return analysisEngineDescriptorPath;
@@ -77,25 +91,23 @@ public class ServiceConfiguration {
 		return serviceType;
 	}
 
+	public void setServiceType(String serviceType) {
+		this.serviceType = serviceType;
+	}
+	
 	public String getAssignedJmxPort() {
 		return assignedJmxPort;
 	}
-
 
 	public String getCustomRegistryClass() {
 		return customRegistryClass;
 	}
 
-
 	public String getCustomProcessorClass() {
 		return customProcessorClass;
 	}
 
-
 	public void validateProperties() throws ServiceInitializationException {
-		if (clientURL == null) {
-			throw new ServiceInitializationException("Client URL not provided - terminating service");
-		}
 		if ( analysisEngineDescriptorPath == null ) {
 			throw new ServiceInitializationException("AE descriptor is missing - unable to launch the service");
 		}
@@ -107,6 +119,7 @@ public class ServiceConfiguration {
 		}
 
 	}
+
 	public void collectProperties(String[] args) {
 		clientURL = System.getProperty("ducc.deploy.JdURL");
 		threadCount = System.getProperty("ducc.deploy.JpThreadCount");
